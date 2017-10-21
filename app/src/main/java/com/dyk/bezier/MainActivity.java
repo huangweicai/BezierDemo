03de0ad425341fffe6f9ed324c83be7241610462
ex_huangwc <>
2017-10-21 13:35:15 +0800

    Signed-off-by: ex_huangwc <黄伟才>

---
+++ app/src/main/java/com/dyk/bezier/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dyk.bezier;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
